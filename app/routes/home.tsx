be686ba6b53dcb792c7fb81783368ce1feb9d276
ori-Balajee <balajee4drip@gmail.com>
2026-06-12 13:00:53 +0530

    Worked on the home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import { resumes } from "../../constants";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "~/components/Navbar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "RECOR" },
+    { name: "description", content: "Welcome to RECOR" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+    <section className = "main-section">
+      <div className="page-heading">
+        <h1>Track Your Application & Resume Rating </h1>
+        <h2> Review your submissions and check AI-powered feedback. </h2>
+      </div>
+    </section>
+
+   {resumes.map((resume) => {
+      <div>
+        <h1>{resume.jobTitle}</h1>
+      </div>
+    ))}
+
+
+
+
+
+
+  </main>
 }
