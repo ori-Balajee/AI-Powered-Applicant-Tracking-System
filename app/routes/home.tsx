2fc335a1b3f17e1b5a3981500d1704df54487740
ori-Balajee <balajee4drip@gmail.com>
2026-06-13 18:25:39 +0530

    Done with the home page UI - Navbar, ResumeCard, ScoreCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,7 @@
-import { resumes } from "../../constants";
+import { resumes } from "../../public";
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -13,22 +14,18 @@ export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
     <section className = "main-section">
-      <div className="page-heading">
+      <div className="page-heading py-16">
         <h1>Track Your Application & Resume Rating </h1>
         <h2> Review your submissions and check AI-powered feedback. </h2>
       </div>
     </section>
 
-   {resumes.map((resume) => {
-      <div>
-        <h1>{resume.jobTitle}</h1>
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume}/>
+        ))}
       </div>
-    ))}
-
-
-
-
-
-
+    )}
   </main>
 }
